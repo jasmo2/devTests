9d3ac748163fbaa3313193d079b9d7a36fb4c2e5
Jaime Molina <js.molina13@uniandes.edu.co>
2018-10-19 23:13:01 -0500

    logenst String

--- longestString.js
+++ longestString.js
@@ -3,3 +3,26 @@
 * and find the longest word within the string.
 * Example string: "What is the longest word in this string"
 */
+
+/**
+ * finds the 1º longest word in the string
+ * @param {string} str
+ * @return {string} longestWord
+ */
+function longestStr(str) {
+    const strArr = str.split(" ");
+    let longestWord = "";
+    let maxCount = -1;
+    for (let index = 0; index < strArr.length; index++) {
+        strArr[index];
+        const strL = strArr[index].length;
+        if (maxCount < strL) {
+            maxCount = strL;
+            longestWord = strArr[index];
+        }
+    }
+
+    return longestWord;
+}
+
+console.log(longestStr("What is the longest word in this string"));
