d807355cb209e771c1f125eba78b557ff96df58b
Jaime Molina <js.molina13@uniandes.edu.co>
2018-10-21 15:52:52 -0500

    Add async

--- async.js
+++ async.js
@@ -9,15 +9,15 @@
  *
  */
 
-var action = function(obj) {
+const action = function (obj) {
     console.log("Started at: " + new Date());
-    setTimeout(function(){
+    setTimeout(function () {
         console.log("Finished at: " + new Date());
         obj.done();
     }, Math.floor(Math.random() * 1000) + 1);
 };
 
-var array = [
+const array = [
     {
         order: 5,
         action: action
@@ -53,11 +53,48 @@ var array = [
 ];
 
 // Code goes here:
+function compare(a, b) {
+    if (a.order < b.order) {
+        return 1;
+    } else if (a.order > b.order) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
 
+function setPromise(el) {
+    return new Promise((resolve, reject) => {
+        el.action({ done: resolve });
+    });
+}
 
+function elValidation(nextEl, el) {
+    return nextEl && (el.order === nextEl.order)
+}
+async function desendingAsync(arr) {
+    const sortedArr = arr.sort(compare)
+    for (let i = 0; i < sortedArr.length; i++) {
+        const el = sortedArr[i];
+        console.log("started order:", el.order);
+        const requests = [setPromise(el)];
+        let nextEl = sortedArr[i + 1];
+        if (elValidation(nextEl, el)) i++;
 
+        while (elValidation(nextEl, el)) {
+            requests.push(setPromise(nextEl));
+            nextEl = sortedArr[i + 1]
+            if (elValidation(nextEl, el)) i++;
+        }
 
+        await Promise.all(requests);
+        console.log("finished order", el.order);
+        console.log("-------");
 
+    }
+}
+
+desendingAsync(array);
 /**
  * Bonus:
  * Order is non-unique, and every action with the same order can be run in parallel.
